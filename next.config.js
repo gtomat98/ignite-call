adaa0ed58442062a1279f97f812535064cada378
Matheus D'Afonseca e Silva <matheus.dafonseca2004@gmail.com>
2023-02-22 21:50:37 -0300

    .

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
   experimental: {
     appDir: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['https://lh3.googleusercontent.com/'],
   },
 }
 
